2f32de0f3945b1114daf76192b33b20f16f58046
Sylvain périé <sylvain.perie@hotmail.fr>
2024-08-23 10:59:50 +0200

    test

---
+++ tuto.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tuto PHP</title>
+</head>
+
+<body>
+    <?php
+    echo "Ceci est un tuto PHP de qualité SUPERIEUUUUUURE";
+    ?>
+
+</body>
+
+</html>
